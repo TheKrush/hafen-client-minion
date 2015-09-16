65f903b728eecf762c33723ca868f6bfe38cb778
TheKrush <krush@lostminions.org>
2015-09-16 00:01:43 -0700

    ignores, and new jars

---
+++ copy-jars.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Copying Files ...
+echo f|xcopy ".\hafen-client\dist\hafen.jar" ".\files\hafen.jar" /R /Y
+echo f|xcopy ".\hafen-minion\dist\hafen-minion.jar" ".\files\hafen-minion.jar" /R /Y
