220f0341dce3c22a1833abc6f7b3c3161294ac9c
TheKrush <krush@lostminions.org>
2015-09-15 23:28:31 -0700

    adding batch for easily testing changes

---
+++ run-test.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Copying Files ...
+echo f | xcopy ".\hafen-client\dist\hafen.jar" ".\hafen-minion\dist\hafen.jar" /R /Y
+if NOT ["%errorlevel%"]==["0"] (
+    pause
+	goto :eof
+)
+cd ".\hafen-minion\dist\"
+echo Running hafen-minion.jar ...
+java -Xms512m -Xmx1024m -jar hafen-minion.jar TESTING
+if NOT ["%errorlevel%"]==["0"] (
+    pause
+)
+cd ".\..\..\"
