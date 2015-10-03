9bcc87bd4d3aaccf2ef8159ef5dc0e265962e10e
TheKrush <krush@lostminions.org>
2015-10-02 22:46:01 -0700

    adding client-res.jar into the mix

--- run-test.bat
+++ run-test.bat
@@ -5,6 +5,11 @@ if NOT ["%errorlevel%"]==["0"] (
     pause
 	goto :eof
 )
+echo f | xcopy ".\hafen-client\dist\client-res.jar" ".\hafen-minion\dist\lib\client-res.jar" /R /Y
+if NOT ["%errorlevel%"]==["0"] (
+    pause
+	goto :eof
+)
 cd ".\hafen-minion\dist\"
 echo Running hafen-minion.jar ...
 java -Xms512m -Xmx1024m -jar hafen-minion.jar TESTING
