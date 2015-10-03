ce4b79896877e623aefce77e8539927a25037ec2
TheKrush <krush@lostminions.org>
2015-10-02 22:52:59 -0700

    2015-10-02 release

--- copy-jars.bat
+++ copy-jars.bat
@@ -1,4 +1,5 @@
 @echo off
 echo Copying Files ...
+echo f|xcopy ".\hafen-client\dist\client-res.jar" ".\files\client-res.jar" /R /Y
 echo f|xcopy ".\hafen-client\dist\hafen.jar" ".\files\hafen.jar" /R /Y
 echo f|xcopy ".\hafen-minion\dist\hafen-minion.jar" ".\files\hafen-minion.jar" /R /Y
